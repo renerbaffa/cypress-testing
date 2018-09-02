857d44d1c6523e54d23e248d6a790d793fa645d6
Rener Baffa da Silva <renerbaffa@reners-mbp.speedport.ip>
2018-09-02 22:55:27 +0200

    Creating test adapter to support redux testing

--- src/helpers/testing.js
+++ src/helpers/testing.js
@@ -1,3 +1,28 @@
+import React from 'react'
 import { fireEvent, render, wait, within } from 'react-testing-library';
+import store from '../store';
+// import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+import {ThemeProvider} from 'styled-components'
+import configureStore from '../store';
 
-export { fireEvent, render, wait, within };
+// const middlewares = [thunk]
+// const mockStore = configureStore(middlewares)
+
+const renderWithRedux = (
+  ui,
+  {initialState = {}, store = configureStore(initialState)} = {},
+) => {
+  const renderedUi = render(
+    <Provider store={store}>
+      {ui}
+    </Provider>
+  );
+
+  return {
+    ...renderedUi,
+    store,
+  };
+}
+
+export { fireEvent, render, wait, within, renderWithRedux };
